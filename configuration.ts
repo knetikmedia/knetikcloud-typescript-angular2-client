9b4799cf871890f74f6da205e81af9896794cc81
youssef <youssef>
2018-06-26 20:58:46 +0200

    CUEN-143 Angular JS SDK JSON parser to convert from underscore property to UpperCase

--- configuration.ts
+++ configuration.ts
@@ -15,13 +15,7 @@ export class Configuration {
     basePath?: string;
     withCredentials?: boolean;
 
-    constructor(configurationParameters: ConfigurationParameters = {}) {
-        this.apiKeys = configurationParameters.apiKeys;
-        this.username = configurationParameters.username;
-        this.password = configurationParameters.password;
-        this.accessToken = configurationParameters.accessToken;
-        this.basePath = configurationParameters.basePath;
-        this.withCredentials = configurationParameters.withCredentials;
+    constructor() {
     }
 
     /**
